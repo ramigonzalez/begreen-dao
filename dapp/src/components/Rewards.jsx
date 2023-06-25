74d350d4bb67482ce8eed330cabff5d02bc96a14
LAPTOP-QOOVCJT1\Dudley Dursley <ramiro.gonzalez@sandbox.game>
2023-06-24 22:45:23 -0300

    reward component

---
+++ dapp/src/components/Rewards.jsx
@@ -0,0 +1,12 @@
+import "./Rewards.css";
+
+const Rewards = ({ totalWaves }) => {
+
+  return (
+    <div className="">
+      <h3>Total waves: {totalWaves}</h3>
+    </div>
+  );
+};
+
+export default Rewards;
